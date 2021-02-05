303c4388d8e1a69691bc2fc420535bdc3f810193
gonzalowebdev <74275793+gonzalowebdev@users.noreply.github.com>
2021-02-04 21:12:02 -0300

    Add files via upload

---
+++ src/configs/DBConnection.js
@@ -0,0 +1,15 @@
+const mysql = require ("mysql2");
+/*
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'passport-example'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Database connected!");
+});
+
+module.exports = connection;*/
